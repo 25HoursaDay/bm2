496d4ad31757e76b716a754c926e54b782ff3db1
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-09-14 10:06:41 -0500

    Create smth.js

---
+++ smth.js
@@ -0,0 +1,9 @@
+  function aboutblank(){
+  let win = window.open();
+  win.document.id = "win";
+  win.document.innerHTML = '<div style=margin-left:auto;margin-right:auto;text-align:center><iframe src="https://rhodium-demo.herokuapp.com/service/https://www.coolmathgames.com/" width=100% height=100% style=border:none></iframe></div>")}</script> <script async src="https://arc.io/widget.min.js#nnUFwP4e"></script><button onclick="aboutblank()">Click me</button>';
+  win.focus();
+    function(){
+  $("win").append('<script> if (window.location == "about:blank"){ alert("complete"); } else {window.location.replace("https://www.google.com/?safe=active&ssui=on"); } </script>');
+                  };
+                  }
