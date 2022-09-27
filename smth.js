d14559734a84edf664749f091d7b70cc828c69cc
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-09-27 11:11:15 -0500

    Update smth.js

--- smth.js
+++ smth.js
@@ -1,5 +1,4 @@
-
   let win = window.open();
-  win.document.write('<div style=margin-left:auto;margin-right:auto;text-align:center><iframe src="https://rhodium-demo.herokuapp.com/service/https://www.coolmathgames.com/" width=100% height=100% style=border:none></iframe></div>")}</script> <script async src="https://arc.io/widget.min.js#nnUFwP4e"></script><button onclick="aboutblank()"</script>');
-  win.append('<script> if (window.location == "about:blank"){ alert("complete!"); } else { win.location.replace("https://www.google.com/?safe=active&ssui=on"); } </script>');
-
+  let url = "";
+  win.document.write('<div style=margin-left:auto;margin-right:auto;text-align:center><iframe src="'+url+'" width=100% height=100% style=border:none></iframe></div>');
+  win.append('<script> if (window.location == "about:blank"){ win.focus(); } else { win.location.replace("https://www.google.com/?safe=active&ssui=on");} </script>');
